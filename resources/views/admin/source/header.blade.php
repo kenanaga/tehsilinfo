635582a605d56188f8128c1d34fa112ff6d1df02
kenanaga <kenanaga1020@gmail.com>
2022-05-01 15:45:47 +0400

    change

--- resources/views/admin/source/header.blade.php
+++ resources/views/admin/source/header.blade.php
@@ -135,6 +135,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </a>
           </li>
           <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Redaktorlar
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
             <a href="{{route('admin.logout')}}" class="nav-link">
               <i class="nav-icon fas fa-th"></i>
               <p>
--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Middleware\isAdmin;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
-
 
 
 Route::get('/post', [AuthController::class, 'post'])->name('admin.post')->middleware('isAdmin');
@@ -19,7 +18,6 @@ Route::post('/addpost', [AuthController::class, 'addPost'])->name('admin.addpost
 Route::post('/update', [AuthController::class, 'update'])->name('post.edit')->middleware('isAdmin');
 Route::get('/update/{id}', [AuthController::class, 'postupdate'])->name('post.update')->middleware('isAdmin');
 Route::get('/delete/{id}', [AuthController::class, 'postdelete'])->name('post.delete')->middleware('isAdmin');
-
 
 Route::get('/login', [AuthController::class, 'login'])->name('admin.login')->middleware('isNotAdmin');
 Route::post('/login', [AuthController::class, 'loginPost'])->name('admin.login.post');
