9818fe40cb44d7721577f7a5d79430a68d3185be
kenanaga <kenanaga1020@gmail.com>
2022-05-19 23:51:43 +0400

    update

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -23,15 +23,15 @@ class FrontController extends Controller
         $data['news']= News::where('id',$id)->get();
         $datas=News::where('id',$id)->first();
         //update olunmalidi
-        DB::table('news')->increment('hit', 1,['slug' => $datas->slug]);
-
+        //DB::table('news')->increment('hit', 1,['id' => $datas->id]);
+        DB::table('news')->where('hit',$datas->hit)->update(['hit' => $datas->hit+1]);
         $data['pspecial']= News::where('category_id',12)->orderBy('created_at','Desc')->lazy();
         $data['related']= News::where('category_id',$datas->category_id)->orderBy('created_at','Desc')->lazy()->skip(0)->take(3);
          return view('front/singlepost',$data);
      }
 
     public function category($cat){
-        $data['pspecial']= News::where('category_id',12)->orderBy('created_at','Desc')->lazy()->skip(0)->take(2);
+        $data['pspecial']= News::where('category_id',12)->orderBy('created_at','Desc')->lazy()->skip(0)->take(4);
         $id=Category::where('slug',$cat)->first();
         $data['cnews']= News::where('category_id',$id->id)->orderBy('created_at','Desc')->simplePaginate(20);
         return view('front/categorynews',$data);
