fe337970b18c0161d64cf9ca8e8f866315a5f5b1
kenanaga <kenanaga1020@gmail.com>
2022-07-05 13:15:50 +0400

    update

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Admin;
@@ -22,9 +21,8 @@ class FrontController extends Controller
     public function singlepost($id){
         $data['news']= News::where('id',$id)->get();
         $datas=News::where('id',$id)->first();
-        //update olunmalidi
-        //DB::table('news')->increment('hit', 1,['id' => $datas->id]);
-        DB::table('news')->where('hit',$datas->hit)->update(['hit' => $datas->hit+1]);
+
+        DB::table('news')->where('id',$datas->id)->update(['hit' => $datas->hit+1]);
         $data['pspecial']= News::where('category_id',12)->orderBy('created_at','Desc')->lazy();
         $data['related']= News::where('category_id',$datas->category_id)->orderBy('created_at','Desc')->lazy()->skip(0)->take(3);
          return view('front/singlepost',$data);
